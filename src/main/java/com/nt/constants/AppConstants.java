6ba07c0d919b3f6f8f4ea270710b4e6e4b795c8a
Sai Kumar <kumarvukkum@gmail.com>
2022-07-05 08:07:23 +0530

    added rest

--- src/main/java/com/nt/constants/AppConstants.java
+++ src/main/java/com/nt/constants/AppConstants.java
@@ -1,8 +1,8 @@
 package com.nt.constants;
 
 public class AppConstants {
-     public static final String Contact_Save_Success="Contact Saved";
-     public static final String Contact_Save_Fail="Contact Failed to Saved";
-     public static final String Contact_Delete_Success="Contact Deleted";
-     public static final String Contact_Delete_Fail="Contact Failed to Delete";
+     public static final String Contact_Save_Success="contactsSaveSucc";
+     public static final String Contact_Save_Fail="contactsSaveFail";
+     public static final String Contact_Delete_Success="contactsDeleteSucc";
+     public static final String Contact_Delete_Fail="contactsDeleteFail";
 }
